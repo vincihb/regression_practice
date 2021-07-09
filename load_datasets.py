bbfe3cd6051c9aa3f61a934cd46d713e9d348aef
Himesh Bhatia <himeshbhatia@live.com>
2021-07-09 15:40:40 -0400

    Shuffling dataset and also correctly implemented IRLS algorithm in evaluate_logistic_1 function

--- load_datasets.py
+++ load_datasets.py
@@ -21,11 +21,27 @@ def estimate_linear(num_points, location="./datasets/linear.npy"):
     plt.show()
 
 
+def shuffle_dataset(x, y):
+    x = list(x)
+    y = list(y)
+    arr = [[], []]
+    while x:
+        index = np.random.randint(0, len(x))
+        x_val = x.pop(index)
+        y_val = y.pop(index)
+        arr[0].append(x_val)
+        arr[1].append(y_val)
+    return arr
+
+
 def estimate_logistic(num_points, location="./datasets/logistic.npy"):
     x, y = np.load(location)
+    shuffled = shuffle_dataset(x, y)
+    x = np.array(shuffled[0])
+    y = np.array(shuffled[1])
     print(num_points)
     mu = []
-    betas = np.random.standard_normal(2)
+    betas = [6, 7]
     n = 0
     while n < len(x):
         x_val = x[n]
@@ -35,19 +51,74 @@ def estimate_logistic(num_points, location="./datasets/logistic.npy"):
             X = np.concatenate((np.array(X), np.array([np.concatenate((np.ones(1), np.array([x_val])))])))
         else:
             X = np.concatenate((np.ones(1), np.array([x_val])))
-        print(X)
-        mu_val = 1.0/(1 + math.exp(- betas[0] + betas[1] * x_val))
+        if n % 100 == 0:
+            print(n)
+            print(betas)
+        mu_val = 1.0 / (1 + math.exp(- betas[0] - betas[1] * x_val))
         mu.append(mu_val)
         Y = np.array(y[0: n + 1])
         S = np.diag([mu_val_temp for mu_val_temp in mu])
         if n == 0:
             print("Here")
-        else:
-            temp = np.linalg.inv(np.matmul(np.matmul(np.transpose(X), S), X))
-            print(temp)
-        if n == 4:
+            betas = [0, 0]
+            n = n + 1
+            continue
+        temp = np.linalg.inv(np.matmul(np.matmul(np.transpose(X), S), X))
+        temp2 = np.matmul(np.matmul(S, X), betas) + Y - mu
+        temp2 = np.matmul(np.transpose(X), temp2)
+        temp2 = np.matmul(temp, temp2)
+        if (betas[0] - temp2[0]) ** 2 + (betas[1] - temp2[1]) ** 2 < 0.000001:
             break
+        betas = temp2.copy()
+        # if n == 4:
+        #     break
+        n = n + 1
+    print(betas)
+
+
+def estimate_logistic_1(num_points, location="./datasets/logistic.npy"):
+    x, y = np.load(location)
+    shuffled = shuffle_dataset(x, y)
+    x = np.array(shuffled[0])
+    y = np.array(shuffled[1])
+    print(num_points)
+    mu = []
+    betas = [0, 0]
+    n = 0
+    while n < len(x):
+        x_val = x[n]
+        if n == 1:
+            X = np.concatenate((np.array([X]), np.array([np.concatenate((np.ones(1), np.array([x_val])))])))
+        elif n > 1:
+            X = np.concatenate((np.array(X), np.array([np.concatenate((np.ones(1), np.array([x_val])))])))
+        else:
+            X = np.concatenate((np.ones(1), np.array([x_val])))
+        mu_val = 1.0 / (1 + math.exp(- betas[0] - betas[1] * x_val))
+        mu.append(mu_val)
         n = n + 1
 
+    iteration = 0
+    while True:
+        if iteration % 100 == 0:
+            print(iteration)
+            print(betas)
+        n = 0
+        while n < len(x):
+            x_val = x[n]
+            mu_val = 1.0 / (1 + math.exp(- betas[0] - betas[1] * x_val))
+            mu[n] = mu_val
+            n = n + 1
+        Y = np.array(y)
+        S = np.diag(mu)
+        temp = np.linalg.inv(np.matmul(np.matmul(np.transpose(X), S), X))
+        temp2 = np.matmul(np.matmul(S, X), betas) + Y - mu
+        temp2 = np.matmul(np.transpose(X), temp2)
+        temp2 = np.matmul(temp, temp2)
+        if math.sqrt((betas[0] - temp2[0]) ** 2 + (betas[1] - temp2[1]) ** 2) < 0.00000001:
+            break
+        iteration = iteration + 1
+        betas = temp2.copy()
+    print(betas)
 
-estimate_logistic(1000000)
+
+estimate_logistic_1(1000000)
--- make_datasets.py
+++ make_datasets.py
@@ -16,8 +16,8 @@ def create_logistic(num_points):
     x = [(a - num_points / 2.0) / (num_points / 20.0) for a in range(0, num_points, 1)]
     epsilon = 0
     # epsilon = np.random.standard_normal(num_points)
-    m = 2.0
-    b = 1.0
+    m = 1.0
+    b = 2.0
     y_temp = [m * x_val + b for x_val in x]
     y_temp = np.array(y_temp) + epsilon
     p = [1.0 / (1 + math.exp(-y_val)) for y_val in y_temp]
